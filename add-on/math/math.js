cd65dc0dd41d827139faa4438904e7f12a36076b
zsz12251665 <zsz12251665@163.com>
2018-11-10 22:53:00 +0800

    Update math add-on for Safari (or Firefox)

--- add-on/math/math.js
+++ add-on/math/math.js
@@ -4,9 +4,9 @@ function LoadMath()
 	//Load the stylesheet of mathmatical expressions
 	document.getElementsByTagName('head')[0].innerHTML+='<link rel="stylesheet" type="text/css" href="/add-on/math/math.css" />';
 	//Search and replace the expressions
-	for(var t=0,math=document.getElementsByTagName('math');t<math.length;t++)
+	for(var t=0,data=document.getElementsByTagName('data');t<data.length;t++)
 	{
-	    var html=math[t].innerHTML;
+	    var html=data[t].value;
 		var stack=new Array();
 		for(var i=0;i<html.length;i++)
 		{
@@ -18,21 +18,21 @@ function LoadMath()
 			if((html[i]>='a')&&(html[i]<='z')&&(i+1<html.length)&&(html[i+1]=='{'))
 			{
 				stack.push(html[i]);
-				html=html.slice(0,i)+'<m'+html[i]+'>'+html.slice(i+2);
-				i+=3;
+				html=html.slice(0,i)+'<span class="'+html[i]+'">'+html.slice(i+2);
+				i+=15;
 			}
 			if(html[i]=='}')
 			{
-				html=html.slice(0,i)+'</m'+stack.pop()+'>'+html.slice(i+1);
-				i+=4;
+				html=html.slice(0,i)+'</span>'+html.slice(i+1);
+				i+=6;
 			}
 		}
-		math[t].innerHTML=html;	
+		data[t].innerHTML=html;	
 	}
 	//Update the font of variable
-	for(var t=0,mv=document.getElementsByTagName('mv');t<mv.length;t++)
+	for(var t=0,data=document.getElementsByClassName('v');t<data.length;t++)
 	{
-	    var html=mv[t].innerHTML;
+	    var html=data[t].innerHTML;
 		for(var i=0;i<html.length;i++)
 		{
 			if((html.charCodeAt(i)>64)&&(html.charCodeAt(i)<91))
@@ -40,6 +40,6 @@ function LoadMath()
 			if((html.charCodeAt(i)>96)&&(html.charCodeAt(i)<123))
 				html=html.slice(0,i)+'&#'+String(119841+html.charCodeAt(i))+';'+html.slice(i+1);
 		}
-		mv[t].innerHTML=html;
+		data[t].innerHTML=html;
 	}
 }
