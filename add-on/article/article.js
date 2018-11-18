54c82f0203ead1d857f5b141b8427df2ca8c17b3
zsz12251665 <zsz12251665@163.com>
2018-11-18 14:02:59 +0800

    Upload theme 4night

--- add-on/article/article.js
+++ add-on/article/article.js
@@ -5,25 +5,20 @@ function LoadArticle()
 	var request=JSON.parse('{'+location.search.slice(1).replace(/([^&=]*)=([^&]*)/g,'"$1":"$2"').replace(/&/g,',')+'}');
 	if(request.theme)
 		document.body.className=request.theme;
-	//Load the stylesheet of the article
+	//Load the style sheet of the article
 	document.getElementsByTagName('head')[0].innerHTML+='<link rel="stylesheet" type="text/css" href="/add-on/article/article.css" />';
 	document.getElementsByTagName('head')[0].innerHTML+='<link rel="stylesheet" type="text/css" href="/theme/'+document.body.className+'.css" />';
 	//Fill the body and load the frame
 	var HTMLCode='';
-	if((location.pathname=='/')||(location.pathname=='/index.html'))
-		HTMLCode+='<a href="/sitemap.html"><header>zsz12251665</header></a>';
-	else
-		HTMLCode+='<a href="/"><header>zsz12251665</header></a>';
-	HTMLCode+='<article>';
+	HTMLCode+='<header>zsz12251665</header>';
 	HTMLCode+='<div style="font-size: xx-large;opacity: 0;padding: 0.1rem;">&nbsp;</div>';
+	HTMLCode+='<article>';
 	HTMLCode+=document.getElementsByTagName('body')[0].innerHTML;
 	HTMLCode+='</article>';
 	HTMLCode+='<footer>&copy; 2016-2018 zsz12251665. Theme by zsz12251665. Licensed under the <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/">CC BY-NC-ND 4.0 International License</a>. </footer>';
 	document.getElementsByTagName('body')[0].innerHTML=HTMLCode;
+	//Load the event listener
+	document.getElementsByTagName('header')[0].addEventListener('click',function(){location.href=((location.pathname=='/')||(location.pathname=='/index.html'))?'/sitemap.html':'/'});
 	for(var i=0,abbr=document.getElementsByTagName('abbr');i<abbr.length;i++)
-		abbr[i].addEventListener('click',ShowTitle);
-}
-function ShowTitle(e)
-{
-	alert(e.target.title);
+		abbr[i].addEventListener('click',function(e){alert(e.target.title);});
 }
