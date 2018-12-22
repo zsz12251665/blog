75f554f8e02bf848a83be1386a123e66918222af
zsz12251665 <zsz12251665@163.com>
2018-12-22 22:15:39 +0800

    Update codes

--- add-on/article/article.js
+++ add-on/article/article.js
@@ -3,11 +3,15 @@ function LoadArticle()
 {
 	//Load the request and change the theme
 	var request=JSON.parse('{'+location.search.slice(1).replace(/([^&=]*)=([^&]*)/g,'"$1":"$2"').replace(/&/g,',')+'}');
+	if(!sessionStorage.getItem('theme'))
+		sessionStorage.setItem('theme','default');
 	if(request.theme)
-		document.body.className=request.theme;
+		sessionStorage.setItem('theme',request.theme);
+	if(sessionStorage.getItem('theme')!='default')
+		document.body.className=sessionStorage.getItem('theme');
 	//Load the style sheet of the article
 	document.getElementsByTagName('head')[0].innerHTML+='<link rel="stylesheet" type="text/css" href="/add-on/article/article.css" />';
-	document.getElementsByTagName('head')[0].innerHTML+='<link rel="stylesheet" type="text/css" href="/theme/'+document.body.className+'.css" />';
+	document.getElementsByTagName('head')[0].innerHTML+='<link rel="stylesheet" type="text/css" href="/add-on/article/theme/'+document.body.className+'.css" />';
 	//Fill the body and load the frame
 	document.getElementsByTagName('body')[0].innerHTML='<header>zsz12251665</header><div style="font-size: xx-large;opacity: 0;padding: 0.1rem;">&nbsp;</div><article>'+document.getElementsByTagName('body')[0].innerHTML+'</article><footer>&copy; 2016-2018 zsz12251665. Theme by zsz12251665. Licensed under the <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/">CC BY-NC-ND 4.0 International License</a>. </footer>';
 	//Load the title
