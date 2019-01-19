cbca60a67ab0f93ff1ba73feb3b3d24f07a651fd
zsz12251665 <zsz12251665@163.com>
2019-01-19 22:56:46 +0800

    Update themes

--- add-on/article/article.js
+++ add-on/article/article.js
@@ -13,7 +13,6 @@ function LoadArticle()
 	document.getElementsByTagName('head')[0].innerHTML+='<link rel="stylesheet" type="text/css" href="/add-on/article/article.css" /><link rel="stylesheet" type="text/css" href="/add-on/article/theme.css" />';
 	//Fill the body and load the frame
 	document.getElementsByTagName('body')[0].innerHTML='<nav class="btnfield"><select id="theme"><option value="default">Default theme</option><option value="bricks">bricks</option><option value="matrix">matrix</option><option value="mytale">mytale</option><option value="nights">nights</option><option value="OParis">OParis</option><option value="vector">vector</option></select><br /><a href="/">Homepage</a><a href="/codes/">Code List</a><a href="/posts/">Post List</a><a href="/sitemap.html">Sitemap</a></nav><header>zsz12251665</header><article>'+document.getElementsByTagName('body')[0].innerHTML+'</article><footer>&copy; 2016-2019 zsz12251665. Theme by zsz12251665. Licensed under the <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/">CC BY-NC-ND 4.0 International License</a>. </footer>';
-	document.getElementsByTagName('article')[0].style.marginTop='3rem';
 	//Load the title
 	if(document.getElementById('title'))
 		document.title=document.getElementById('title').innerText+' | zsz12251665\'s Blog';
