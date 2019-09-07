3c082fd79b52f978eb7c7e22a6620d1aa967c3af
zsz12251665 <zsz12251665@163.com>
2019-09-08 02:50:39 +0800

    Upload CF 1214D

--- add-on/math/math.js
+++ add-on/math/math.js
@@ -6,7 +6,7 @@ function LoadMath() {
 	document.head.innerHTML += '<link rel="stylesheet" type="text/css" href="/add-on/math/math.css" />';
 	// Load the expressions
 	for (let data of document.querySelectorAll('data'))
-		data.innerHTML = data.value.replace(/([a-z])\{/g, '<span class="$1">').replace(/(?<!\\)\}/g, '</span>').replace(/\\(.)/g, '$1');
+		data.innerHTML = data.value.replace(/\\(.)/g,'$1\\').replace(/([a-z])\{/g, '<span class="$1">').replace(/\}(?!\\)/g, '</span>').replace(/(.)\\/g, '$1');
 	// Update font styles of variables
 	for (let variable of document.querySelectorAll('data span.v'))
 		for (let i = 0; i < 26; i++) {
