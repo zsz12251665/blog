446b3a8922f08ea23207f66928fcd61ce89d5853
zsz12251665 <zsz12251665@163.com>
2020-01-27 23:32:50 +0800

    Update the math add-on

--- add-on/math/math.js
+++ add-on/math/math.js
@@ -5,7 +5,21 @@ document.head.innerHTML += '<link rel="stylesheet" type="text/css" href="/add-on
 
 function LoadMath()
 {
+	// Load the request
+	var request = JSON.parse('{' + location.search.slice(1).replace(/([^&=]*)=([^&]*)/g, '"$1":"$2"').replace(/&/g, ',') + '}');
+	// Initialize the session storage value of large brackets
+	sessionStorage.setItem('math.largeBrackets', request.largeBrackets || sessionStorage.getItem('math.largeBrackets') || 'false');
 	// Load the expressions
 	for (let data of document.querySelectorAll('data'))
 		data.innerHTML = data.value.replace(/\\(.)/g, '$1\\').replace(/([a-z])\{(?!\\)/g, '<span class="$1">').replace(/\}(?!\\)/g, '</span>').replace(/(.)\\/g, '$1');
+	//Scale the bracket beside the matrix
+	if (sessionStorage.getItem('math.largeBrackets') == 'true')
+		for (let data of document.querySelectorAll('data'))
+			for (let matrix of data.querySelectorAll('span.m'))
+			{
+				if (matrix.nextSibling && matrix.nextSibling.innerText.match(/[\(\)\[\]\|]/))
+					matrix.nextSibling.style.fontSize = String(matrix.querySelectorAll('span.l:empty').length + 1) + 'em';
+				if (matrix.previousSibling && matrix.previousSibling.innerText.match(/[\(\)\[\]\|]/))
+					matrix.previousSibling.style.fontSize = String(matrix.querySelectorAll('span.l:empty').length + 1) + 'em';
+			}
 }
